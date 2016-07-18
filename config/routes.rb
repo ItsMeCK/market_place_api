80b335b00d486dadaa814c430c941147ef37682b
chandrakant <chandrakant7892@gmail.com>
2016-07-18 23:14:46 +0530

    Active record serialiser

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json } do
     scope module: :v1 do
-      resources :users, :only => [:show, :create, :update, :destroy]
+      resources :users, :only => [:show, :create, :update, :destroy] do
+        # this is the line
+        resources :products, :only => [:create, :update, :destroy]
+      end
+      resources :sessions, :only => [:create, :destroy]
+      resources :products, :only => [:show, :index]
     end  
   end  
 end
